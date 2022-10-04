b010876cc187656dead4f6c5c51d2590dbcb6cbd
Aquilae00 <linaldi03@gmail.com>
2022-10-04 05:35:27 -0400

    init

---
+++ utils/poi.ts
@@ -0,0 +1,15 @@
+export const POI = [
+  { name: 'TOLLAVISTE', long: -73.037319, lat: 42.063911 },
+  { name: 'UN COMPOUND', long: -73.038383, lat: 42.062492 },
+  { name: 'CROSSROADS TOWER', long: -73.0394, lat: 42.063531 },
+  { name: 'SAM SITE', long: -73.039989, lat: 42.064361 },
+  { name: 'BUNKER HILL', long: -73.040061, lat: 42.06275 },
+  { name: 'KONTRA VALLEY', long: -73.040389, lat: 42.061839 },
+  { name: 'FUEL DEPOT', long: -73.041811, lat: 42.063019 },
+  { name: 'BORDER FORTRESS/SHIRE', long: -73.043211, lat: 42.06325 },
+  { name: 'DOCKYARD', long: -73.042831, lat: 42.061681 },
+  { name: 'SMUGGLERS BAY', long: -73.04095, lat: 42.060639 },
+  { name: 'SAP HOUSE HILL', long: -73.038469, lat: 42.061789 },
+  { name: 'GRANVISTE RUINS', long: -73.03665, lat: 42.061119 },
+  { name: 'TZAGA-LUNCI', long: -73.038819, lat: 42.063189 }
+];
